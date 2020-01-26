30e8f0c370a91d3299ef5f6a6bf3b755453b5b89
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-26 01:13:13 +0000

    Done.

--- oksana.CLI_project/lib/CLI_project/games.rb
+++ oksana.CLI_project/lib/CLI_project/games.rb
@@ -1,6 +1,13 @@
 Class Games 
-  attr_accessor :title, :developer, :genre 
+  attr_accessor :title, :developer, :genre, :description
   
 end 
+
+def first_title_url 
+  self.first_title_url
+ end 
+
+end 
+
 
 
