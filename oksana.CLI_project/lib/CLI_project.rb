35b39cf87cce2d8c0a90bd577bd353a5ba92eb03
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-26 00:36:19 +0000

    Done.

--- oksana.CLI_project/lib/CLI_project.rb
+++ oksana.CLI_project/lib/CLI_project.rb
@@ -1,6 +1,11 @@
-require "CLI_project/version"
+Class ::CLI
 
-module CLIProject
-  class Error < StandardError; end
-  # Your code goes here...
-end
+def begin 
+  main_menu 
+end 
+
+def main_menu
+  puts <<steamstore.com>> 
+  "Welcome to your steamstore where you can access a wide variety of games based on your genre preference." 
+  "This application will select games based on the genre you want" 
+  "We will select a few games based on the genre you select" 
