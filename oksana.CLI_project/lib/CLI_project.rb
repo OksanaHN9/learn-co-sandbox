66dd863a26fff40e6c74bd38fe4f5e48da390005
Oksana Nichols <oksanahnichols@yahoo.com>
2020-01-27 01:58:12 +0000

    Done.

--- oksana.CLI_project/lib/CLI_project.rb
+++ oksana.CLI_project/lib/CLI_project.rb
@@ -13,8 +13,11 @@ def call
 def list_genre
  puts "Lists four genres to choose from"
  @genres = Game_Genre::Genre.menu 
- @genres.each.with_index(4) do |genres, i|
+ @genres.each.with_index(1) do |genres, i|
    puts "#{i}". #{genre.action} #{genre.horror} #{genre.fps} #{genre.mmo} 
   end 
 end 
  
+def title 
+  puts "Here are a list of titles based on the genre you selected"
+  
